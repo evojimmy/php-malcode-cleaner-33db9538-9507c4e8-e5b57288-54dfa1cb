7883949e6538c714fcc5b968b07279ab6f63a2a5
evojimmy <evolution.jimmy@gmail.com>
2014-01-30 12:49:32 +0800

    empty the file when corruption detected

--- __cleaner.php
+++ __cleaner.php
@@ -15,9 +15,15 @@ function remove_injected_header ($filename) {
   $fh_str = file_get_contents($filename);
   if (preg_match('/^<\?php \$[a-z]{10} = \'/', $fh_str)) {
     echo_log("INJECTED");
-    $clean_str = preg_replace('/^<\?php \$[a-z]{10} = \'(?:.*?)-1; \?>/', '', $fh_str, 1); // limit once
-    //echo $clean_str;//die();
-    echo_log("&nbsp;&nbsp;&nbsp;&nbsp;cleaning...");
+    if (preg_match('/^<\?php \$[a-z]{10} = \'(?:.*?)-1; \?>/', $fh_str)) {
+      // We can determine the malcode-block and preserve good codes.
+      $clean_str = preg_replace('/^<\?php \$[a-z]{10} = \'(?:.*?)-1; \?>/', '', $fh_str, 1); // limit once
+      echo_log("&nbsp;&nbsp;&nbsp;&nbsp;we can clean it...");
+    } else {
+      // We cannot determine because of file corruption. Just empty the file.
+      $clean_str = '<!-- File emptied by php_malcode_cleaner because of corruption. -->';
+      echo_log("&nbsp;&nbsp;&nbsp;&nbsp;file corrupted, empty the file...");
+    }
     rename($filename, $filename.".cleaner__backup"); // make a backup
     if (file_put_contents($filename, $clean_str)) {
       echo_log("done");
